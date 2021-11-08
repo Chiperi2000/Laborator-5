1af3581cbac1c3718ca6b8bd7830e48d05bf9ea6
Chiperi2000 <iulianchiperi2000@gmail.com>
2021-11-08 16:06:37 +0200

    rezolvat

---
+++ Ex8.py
@@ -0,0 +1,10 @@
+#What is the output and what it represents?
+
+myList = [17, 3, 11, 5, 1, 9, 7, 15, 13] 
+largest = myList[0] 
+for i in myList: 
+    if i > largest: 
+        largest = i 
+print(largest)
+
+#reprezinta cel mai mare numar din lista
