bb247060c33f5e05224a7e95c39e73ce7cfd6001
Chiperi2000 <iulianchiperi2000@gmail.com>
2021-11-11 12:43:04 +0200

    rezolvat

---
+++ Ex10.py
@@ -0,0 +1,9 @@
+numere = [5, 11, 9, 42, 3, 49] 
+numere_alese = [3, 7, 11, 42, 34, 49] 
+numar_gasit = 0 
+for numar in numere_alese: 
+    if numar in numere:
+        numar_gasit += 1 
+print(numar_gasit)
+
+
